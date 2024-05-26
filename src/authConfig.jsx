6ab075131d656ad7581e7cbc7a60b76b26329809
Shivansh Mitra <shivanshmitra53@gmail.com>
2024-05-26 22:26:04 +0530

    auth

--- src/authConfig.jsx
+++ src/authConfig.jsx
@@ -2,7 +2,7 @@ export const msalConfig = {
     auth: {
       clientId: "087d5427-2255-48c9-869e-9428f0a88b20", 
       authority: "https://login.microsoftonline.com/5030b96a-04c0-4332-92a2-b935cadbb142", 
-      redirectUri: "https://rhc-dashboard.vercel.app/", 
+      redirectUri: "https://rhc-internal.vercel.app/", 
     },
     cache: {
       cacheLocation: "sessionStorage", 
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -22,3 +22,4 @@ const Login = () => {
 };
 
 export default Login;
+
