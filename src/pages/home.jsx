4989b8546ed079549efe9fdc9ae9aee2fbfa0995
Shivansh Mitra <shivanshmitra53@gmail.com>
2024-05-30 15:10:38 +0530

    aunnouncemnet apis

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -29,6 +29,7 @@ const Home = () => {
   const [news, setNews] = useState([]);
   const [newEmployee, setNewEmployee] = useState([]);
   const [upcomingEvents, setUpcomingEvents] = useState([]);
+  const [gallery,setGallery] = useState([]);
 
   console.log(accounts)
   useEffect(() => {
@@ -73,7 +74,8 @@ const Home = () => {
             { name: 'Employee Directory', id: '50e093d8-d366-4994-a9d8-ac460cb6e18a', setStateFunction: setEmployeeDirectory },
             { name: 'News', id: '0304b663-8abb-414e-a03c-2d7f00cff357', setStateFunction: setNews },
             { name: 'New Employee', id: 'cc29e416-2bf1-4462-8d41-d2b437357776', setStateFunction: setNewEmployee },
-            { name: 'Upcoming events', id: 'fd974e0a-d601-4921-804c-10ff956619e2', setStateFunction: setUpcomingEvents }
+            { name: 'Upcoming events', id: 'fd974e0a-d601-4921-804c-10ff956619e2', setStateFunction: setUpcomingEvents },
+            { name: 'Gallery', id: '9505ceb4-ece5-447d-99fa-b383a324dcd9', setStateFunction: setGallery },
         ];
 
         lists.forEach(list => {
