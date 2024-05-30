360b3ed05bfd4220363553f9a72de5bc78d258c8
i-am-SnehaChauhan <sneha2003chauhan@gmail.com>
2024-05-30 22:32:23 +0530

    Announcement Added

--- src/components/Announcement.jsx
+++ src/components/Announcement.jsx
@@ -6,8 +6,7 @@ const Announcement = ({announcement}) => {
     return <div>No announcement available.</div>; 
   }
 
-  const Title = announcement.value.fields.Title;
-  const preview_en = announcement.value.fields.preview_en;
+  const { Title, preview_en } = announcement.value[0].fields;
   console.log('Announcement-Tile:', Title);
   console.log('Announcement-Preview:', preview_en);
 
@@ -15,8 +14,8 @@ const Announcement = ({announcement}) => {
     <div className="w-full justify-center bg-[#50917F] p-[20px] flex flex-wrap rounded-lg gap-[30px] items-center">
       <div className="w-[460px] h-[320px] bg-slate-300 rounded-lg overflow-hidden"></div>
       <div className="w-[401px] text-white">
-        <h1 className="text-[24px] font-light mb-[10px]">Annoucement Title Here</h1>
-        <p className="text-[16px] font-light mb-[30px]"> Announcement Preview Here</p>
+        <h1 className="text-[24px] font-light mb-[10px]">{Title}</h1>
+        <p className="text-[16px] font-light mb-[30px]">{preview_en}</p>
         <a
           className="text-[14px] px-[10px] py-[5px] border-[1px] border-white rounded-md"
           href=""
