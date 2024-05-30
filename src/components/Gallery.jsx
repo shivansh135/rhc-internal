24c1d6f10d070b2ad9f874f9b808b65f0ed32c21
i-am-SnehaChauhan <sneha2003chauhan@gmail.com>
2024-05-31 00:03:47 +0530

    Gallery Added

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,4 +1,8 @@
-const Gallery = () => {
+const Gallery = ({ gallery }) => {
+  
+  if (!gallery || gallery.length === 0) {
+    return <div>No gallery available.</div>;
+  }
   return (
     <div className="pt-[30px] pb-[40px] px-[41px] w-full bg-[#C2AB80]">
       <div className="w-full flex justify-between items-center mb-[30px] text-[white]">
@@ -13,45 +17,51 @@ const Gallery = () => {
       
 
       <div className="flex gap-[41.88px] flex-wrap">
-      <div className="w-[404px] min-h-[355px] bg-white rounded-lg overflow-hidden">
+      {gallery.map((item, index) => (
+      <div key={index} className="w-[404px] min-h-[355px] bg-white rounded-lg overflow-hidden">
+        <div className="w-full h-[180px] bg-slate-300">
+        <img src={`https://riyadhholding.sharepoint.com/sites/Shamil/${item.image_name}`} alt={item.Title} className="object-cover w-full h-full" />
+        </div>
         <div className="w-full h-[180px] bg-slate-300"></div>
         <div className="px-[27.88px] mt-[20px]">
-          <h1 className="text-[16px] mb-[5px]">Event Name Here</h1>
+          <h1 className="text-[16px] mb-[5px]">{item.fields.Title}</h1>
           <p className="text-[14px]m font-light text-[#888888]">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry.
+          {item.fields.subtitle_en}
           </p>
 
           <button className="text-[14px] text-white font-light px-[20px] py-[10px] bg-[#3B729C] mt-[20px] rounded-md">View Gallery</button>
         </div>
       </div>
-
-      <div className="w-[404px] min-h-[355px] bg-white rounded-lg overflow-hidden">
-        <div className="w-full h-[180px] bg-slate-300"></div>
-        <div className="px-[27.88px] mt-[20px]">
-          <h1 className="text-[16px] mb-[5px]">Event Name Here</h1>
-          <p className="text-[14px]m font-light text-[#888888]">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry.
-          </p>
-
-          <button className="text-[14px] text-white font-light px-[20px] py-[10px] bg-[#3B729C] mt-[20px] rounded-md">View Gallery</button>
-        </div>
+      ))}
       </div>
 
-      <div className="w-[404px] min-h-[355px] bg-white rounded-lg overflow-hidden">
-        <div className="w-full h-[180px] bg-slate-300"></div>
-        <div className="px-[27.88px] mt-[20px]">
-          <h1 className="text-[16px] mb-[5px]">Event Name Here</h1>
-          <p className="text-[14px]m font-light text-[#888888]">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry.
-          </p>
 
-          <button className="text-[14px] text-white font-light px-[20px] py-[10px] bg-[#3B729C] mt-[20px] rounded-md">View Gallery</button>
-        </div>
-      </div>
-      </div>
+      {/* // <div className="w-[404px] min-h-[355px] bg-white rounded-lg overflow-hidden">
+      //   <div className="w-full h-[180px] bg-slate-300"></div>
+      //   <div className="px-[27.88px] mt-[20px]">
+      //     <h1 className="text-[16px] mb-[5px]">Event Name Here</h1>
+      //     <p className="text-[14px]m font-light text-[#888888]">
+      //       Lorem Ipsum is simply dummy text of the printing and typesetting
+      //       industry.
+      //     </p>
+
+      //     <button className="text-[14px] text-white font-light px-[20px] py-[10px] bg-[#3B729C] mt-[20px] rounded-md">View Gallery</button>
+      //   </div>
+      // </div>
+
+      // <div className="w-[404px] min-h-[355px] bg-white rounded-lg overflow-hidden">
+      //   <div className="w-full h-[180px] bg-slate-300"></div>
+      //   <div className="px-[27.88px] mt-[20px]">
+      //     <h1 className="text-[16px] mb-[5px]">Event Name Here</h1>
+      //     <p className="text-[14px]m font-light text-[#888888]">
+      //       Lorem Ipsum is simply dummy text of the printing and typesetting
+      //       industry.
+      //     </p>
+
+      //     <button className="text-[14px] text-white font-light px-[20px] py-[10px] bg-[#3B729C] mt-[20px] rounded-md">View Gallery</button>
+      //   </div>
+      // </div>
+      // </div> */}
     </div>
   );
 };
